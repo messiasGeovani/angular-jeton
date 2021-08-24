f7777593c0f929c758673a2e2c3c28a37f3639ba
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-24 20:31:57 -0300

    business section config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BusinessSectionModule } from './business-section/business-section.module';
 import { EntrySectionModule } from './entry-section/entry-section.module';
 import { NavbarModule } from './navbar/navbar.module';
 
@@ -11,6 +12,7 @@ import { NavbarModule } from './navbar/navbar.module';
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     NavbarModule,
     EntrySectionModule,
+    BusinessSectionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
