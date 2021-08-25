8c2c34d1c86c530e78e0fea66e932da45a32924b
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-24 22:22:27 -0300

    download section create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BusinessAccountSectionModule } from './business-account-section/business-account-section.module';
+import { DownloadSectionModule } from './download-section/download-section.module';
 import { EntrySectionModule } from './entry-section/entry-section.module';
 import { NavbarModule } from './navbar/navbar.module';
 import { PaymentSectionModule } from './payment-section/payment-section.module';
@@ -15,6 +16,7 @@ import { PaymentSectionModule } from './payment-section/payment-section.module';
     EntrySectionModule,
     BusinessAccountSectionModule,
     PaymentSectionModule,
+    DownloadSectionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/download-section/download-section.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DownloadSectionComponent } from './components';
+
+@NgModule({
+  declarations: [DownloadSectionComponent],
+  imports: [CommonModule],
+  exports: [DownloadSectionComponent],
+})
+export class DownloadSectionModule {}
