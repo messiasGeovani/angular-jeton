343bab6e3af9e861b2f498b8b485203baf0fade1
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-25 22:45:10 -0300

    dropdown menu config

--- src/app/navbar/components/navbar.component.ts
+++ src/app/navbar/components/navbar.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public handleOpenDropdown() {
+    const dropdownElement: Element | null =
+      document.getElementById('dropdown_menu');
+
+    const isHidden = dropdownElement?.classList.contains('mobile-hide');
+
+    if (isHidden) {
+      dropdownElement?.classList.remove('mobile-hide');
+      dropdownElement?.classList.add('sshow');
+    } else {
+      dropdownElement?.classList.remove('sshow');
+      dropdownElement?.classList.add('mobile-hide');
+    }
   }
-
 }
