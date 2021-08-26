7d4e616fba2fd06292833a11ffe52b304ceb7ffa
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-25 21:20:28 -0300

    footer modal config

--- src/app/footer-section/components/footer-section.component.ts
+++ src/app/footer-section/components/footer-section.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer-section',
   templateUrl: './footer-section.component.html',
-  styleUrls: ['./footer-section.component.scss']
+  styleUrls: ['./footer-section.component.scss'],
 })
 export class FooterSectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleOpenModal() {
+    const modal: Element | null = document.getElementById('login_modal_footer');
+
+    modal?.classList.remove('hhidden');
+    modal?.classList.add('sshow');
   }
 
+  handleCloseModal() {
+    const modal: Element | null = document.getElementById('login_modal_footer');
+
+    modal?.classList.remove('sshow');
+    modal?.classList.add('hhidden');
+  }
 }
--- src/app/footer-section/footer-section.module.ts
+++ src/app/footer-section/footer-section.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterSectionComponent } from './components';
+import { ModalModule } from '../modal/modal.module';
 
 @NgModule({
   declarations: [FooterSectionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ModalModule],
   exports: [FooterSectionComponent],
 })
 export class FooterSectionModule {}
